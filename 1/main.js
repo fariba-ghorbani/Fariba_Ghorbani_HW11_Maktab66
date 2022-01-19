51323f558367fae2ee393ca13892e4522e73bf39
Fariba <fariba.ghorbani2127@gmail.com>
2022-01-20 00:07:23 +0330

    HW11 is uploaded

---
+++ 1/main.js
@@ -0,0 +1,10 @@
+let appBodyContent = document.querySelector('.app-body-content')
+function createNewTask (text) {
+    let myDiv = document.createElement('div')
+
+}
+function createNewTask(ele) {
+    if(event.key === 'Enter') {
+        let myDiv = document.createElement('div')     
+    }
+}
